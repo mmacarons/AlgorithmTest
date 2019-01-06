41646f5156873d6cb23a0f01ef463739f8743180
manymany.macarons <manymany.macarons@gmail.com>
2019-01-06 15:37:43 +0900

    1번문제 출제 - 두개의 숫자의 합 구해주기

---
+++ app/src/main/java/kr/tjit/algorithmtest/Question01Activity.java
@@ -0,0 +1,34 @@
+package kr.tjit.algorithmtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+문제 : 두개의 숫자를 입력받아서 버튼을 누르면 합을 띄워주자.
+ */
+public class Question01Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question01);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
