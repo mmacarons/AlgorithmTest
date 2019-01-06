9111f1ffde9b60bd9aedef97611a7351f245ea2e
manymany.macarons <manymany.macarons@gmail.com>
2019-01-06 15:57:45 +0900

    1번문제 정답

--- app/src/main/java/kr/tjit/algorithmtest/Question01Activity.java
+++ app/src/main/java/kr/tjit/algorithmtest/Question01Activity.java
@@ -1,12 +1,20 @@
 package kr.tjit.algorithmtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /*
 문제 : 두개의 숫자를 입력받아서 버튼을 누르면 합을 띄워주자.
  */
 public class Question01Activity extends BaseActivity {
+
+    private android.widget.EditText firstNumEdt;
+    private android.widget.EditText secondNumEdt;
+    private android.widget.Button calculateBtn;
+    private android.widget.TextView resultTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,28 @@ public class Question01Activity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                계산 버튼이 눌리면? 숫자 두개를 int로 따내서 result에 출력
+
+//                첫번째 숫자를 String 형태로 따냄
+                String firstNumStr = firstNumEdt.getText().toString();
+
+//                따낸 String을 int 형태로 변환 => integer.parse의 기능
+                int firstNum = Integer.parseInt(firstNumStr);
+
+                String secondNumStr = secondNumEdt.getText().toString();
+                int secondNum = Integer.parseInt(secondNumStr);
+
+                int result = firstNum + secondNum;
+
+                resultTxt.setText(String.valueOf(result));
+
+            }
+        });
+
     }
 
     @Override
@@ -29,6 +59,9 @@ public class Question01Activity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.resultTxt = (TextView) findViewById(R.id.resultTxt);
+        this.calculateBtn = (Button) findViewById(R.id.calculateBtn);
+        this.secondNumEdt = (EditText) findViewById(R.id.secondNumEdt);
+        this.firstNumEdt = (EditText) findViewById(R.id.firstNumEdt);
     }
 }
